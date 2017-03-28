3fc18994bb2f938ab90c0a6d56d3782ff7458bf5
luogeger <xiaoqing1232@163.com>
2017-03-28 23:16:27 +0800

    theater list finish. pages start

--- 01.douban/modules/theater/module.js
+++ 01.douban/modules/theater/module.js
@@ -2,12 +2,15 @@
     var app = angular.module('theaterApp', []);
 
     app.controller('theaterCtrl', function ($scope, jsonSer){
-        var url = 'https://api.douban.com/v2/book/1220562';
+        var url = 'https://api.douban.com/v2/movie/in_theaters';
         var pararms = {api: '00fa6c0654689a0202ef4412fd39ce06'};
         jsonSer.jsonp(url, pararms, function (data){
-            console.log(data);
+            $scope.title = data.title;
+            $scope.subjects = data.subjects;
+
+
+
+            $scope.$apply();//更新$scope
         })
-
-
     }); //controller - end
 })(angular);
