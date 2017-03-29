70cbca357c795e156b6dad4c94230d65234c1847
luogeger <xiaoqing1232@163.com>
2017-03-29 14:27:01 +0800

    subject page display is successful.

--- 01.douban/modules/subject/module.js
+++ 01.douban/modules/subject/module.js
@@ -1,6 +1,17 @@
 ;(function (angular){
     var app = angular.module('subjectApp', []);
-    app.controller('subjectCtrl', function ($scope, $routeParams){
-        $scope.name = $routeParams;
+    app.controller('subjectCtrl', function ($scope, $routeParams, jsonSer){
+        $scope.name = $routeParams.id;
+
+        var url = 'https://api.douban.com/v2/movie/subject/' + $routeParams.id;
+        var pararms = {
+            api: '00fa6c0654689a0202ef4412fd39ce06'
+        };
+
+        jsonSer.jsonp(url, pararms, function (data){
+            $scope.data = data;
+            //$scope.img = data.images.large;
+            $scope.$apply();//更新$scope
+        });
     })
 })(angular);
